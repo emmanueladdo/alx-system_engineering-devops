13ceea4c4ee7997930efef8febba529b108b7386
root <root@DESKTOP-KUT53MR>
2023-08-25 23:50:55 +0000

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,8 +2,9 @@
 
 file { '/tmp/school':
   ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  mode    => '0744',
-  content => 'I love Puppet',
+  content => 'I love Puppet'
 }
