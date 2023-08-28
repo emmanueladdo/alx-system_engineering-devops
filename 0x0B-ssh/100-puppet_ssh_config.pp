6b9a5114ac6e26bbff77a45fe20b2ea5fa098d81
root <root@DESKTOP-KUT53MR>
2023-08-28 13:03:36 +0000

    puppet configuration

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,15 @@
 #puppet that edits the configuration file 
 
-exec { 'Turn off passwd auth':
-  command => 'bash -c "echo PasswordAuthentication no >> /etc/ssh/ssh_config"',
-  path    => '/usr/bin:/usr/sbin:/bin'
+file_line { 'Turn off passwd authentication':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
 }
-exec { 'Declare identity file':
-  command => 'bash -c "echo IdentityFile \'~/.ssh/school\' >> /etc/ssh/ssh_config"',
-  path    => '/usr/bin:/usr/sbin:/bin'
-}
-exec { 'Turn on pubkey auth':
-  command => 'bash -c "echo PubkeyAuthentication yes >> /etc/ssh/ssh_config"',
-  path    => '/usr/bin:/usr/sbin:/bin'
+
+file_line { 'set identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
