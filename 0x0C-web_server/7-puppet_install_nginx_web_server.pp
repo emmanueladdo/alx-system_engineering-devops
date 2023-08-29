a80c46538f186744a37a76a8a7b117335b9726e4
root <root@DESKTOP-KUT53MR>
2023-08-29 22:51:06 +0000

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,40 +1,21 @@
 # Nginx server setup and configuration
 # Define class to install and configure Nginx
-
-class nginx_server {
-  package { 'nginx':
-    ensure => 'installed',
-  }
-
-  file { '/var/www/html/index.html':
-    content => 'Hello World!',
-  }
-
-  file { '/etc/nginx/sites-available/default':
-    ensure  => 'file',
-    content => "
-      server {
-        listen 80;
-        server_name _;
-        location / {
-          root   /var/www/html;
-          index  index.html;
-        }
-        location /redirect_me {
-          return 301 https://github.com/emmanueladdo;
-        }i
-      }
-    ",
-  }
-
-  service { 'nginx':
-    ensure  => 'running',
-    enable  => true,
-    require => Package['nginx'],
-  }
+package { 'nginx':
+  ensure => installed,
 }
 
-# Apply the class to the node
-node 'default' {
-  class { 'nginx_server': }
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/emmanueladdo permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
