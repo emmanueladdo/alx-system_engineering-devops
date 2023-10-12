d737329c001e63288408c5ac7aa4fbb2863cd86f
root <root@DESKTOP-KUT53MR>
2023-10-12 02:01:42 +0000

    trace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,10 +2,5 @@
 
 exec {'fix-estension':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/bin:/usr/sbin:/bin',
-  require => Exec['restart-apache']
-}
-
-exec {'restart-apache':
-  command => 'sudo service apache2 restart',
+  path    => '/usr/local/bin/:bin/'
 }
